f92c16987b8dc5b52f7b3b6b7f121eae0b849a0d
Yonatan Ayalon <yonatanayalon1@gmail.com>
2019-10-20 23:38:35 +0300

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "US East (N. Virginia)",
+        BUCKET: "yonatan-notes-app-uploads"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://lhs8rmkicd.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_mSLRTCbOq",
+        APP_CLIENT_ID: "590e06pqlhf4jghbcdnl9km1ck",
+        IDENTITY_POOL_ID: "us-east-2:656605a8-e7c0-4e4f-b2d3-ba3a11597b2a"
+    }
+};
